842c29de39332ee882935d429f804401456cf021
Felix Polo <brunolok@gmail.com>
2024-09-30 15:51:40 -0300

    se empeso las la creacion de las validaciones de los campos

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongo from "mongoose";
+import z from "zod";
 
 const userModel = new mongo.Schema({
   usernames: { type: String, required: true, trim: true },
@@ -14,3 +15,15 @@ const userModel = new mongo.Schema({
 });
 
 export const user = mongo.model("User", userModel);
+
+const userModel = z.object({
+  usernames: z.string().min(5, "The username must contain at least 5 characters").trim(),
+  passwords: z.string().min(8, "Password is required").trim(),
+  emails: z.string().min(11, "Email must be at least 11 characters long").email("Email is required is not valid").trim(),
+  profilePicture: z
+    .object({
+      _id: z.string().default("imagenProyect/afpdiox30acmlfvcskww"),
+      url: z.string().url("URL no válida").default("https://res.cloudinary.com/ddwriwzgm/image/upload/v1727374339/imagenProyect/afpdiox30acmlfvcskww.jpg"),
+    })
+    .optional(),
+});
