3b255b7660d017b228466ae3eb3a7041393dfd1f
Felix Polo <brunolok@gmail.com>
2024-09-30 12:07:03 -0300

    union de las dos ramas de main con la de lucas-branch

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import color from "chalk";
 import { PORT, SECRET_KEY } from "./config/config.js";
+import database from "./db/database.js";
 import userRouter from "./routers/user.routes.js";
 import session from "express-session";
 import cookieParser from "cookie-parser";
@@ -15,11 +16,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: [
-      "http://localhost:5500",
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-    ],
+    origin: ["http://localhost:5500", "http://localhost:3000", "http://127.0.0.1:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
@@ -42,7 +39,7 @@ app.use(cookieParser());
 app.use(userRouter);
 app.use(publicationsRoutes);
 app.use(mediaRouter);
-
+database();
 app.listen(PORT, () => {
   console.log(color.blue("server is running in http://localhost:4000"));
 });
