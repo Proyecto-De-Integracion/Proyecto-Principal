ac1f3fae8259c5242b9f2d01a8c056fd9fc47aa7
Felix Polo <brunolok@gmail.com>
2024-10-01 13:59:13 -0300

    se implemento la ruta de actualizacion del usuario correctamnet

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import { user } from "../models/user.model.js";
 import color from "chalk"; // Assuming the model is named User.js
 import generateJWT from "../helpers/generateJWT.js";
+import { uploadImage } from "../helpers/cloudinary.js";
 
 export const register = async (req, res) => {
   try {
@@ -15,7 +16,13 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.log(color.red("                                   error in user registration"));
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.error();
     console.error(error);
+    console.error();
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
     res.status(500).json({ message: "An error occurred during registration" });
   }
 };
@@ -42,9 +49,14 @@ export const login = async (req, res) => {
 
     res.status(200).json({ message: "Login successful" });
   } catch (error) {
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.log(color.red("                                        User login error"));
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.error();
     console.error(error);
-    console.error("--------------------------------");
-    console.error(color.red("Unexpected Error"));
+    console.error();
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    res.status(500).json({ message: "login error" });
   }
 };
 
@@ -59,7 +71,14 @@ export const secureAccess = (req, res) => {
       });
     }
   } catch (error) {
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.log(color.red("                                        User session error"));
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.error();
     console.error(error);
+    console.error();
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    res.status(500).json({ message: "session error" });
   }
 };
 
@@ -74,7 +93,46 @@ export const logout = (req, res) => {
       return res.json({ message: "Cierre de sesión exitoso" });
     });
   } catch (error) {
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.log(color.red("                               An error occurred while closing the session"));
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.error();
     console.error(error);
-    return res.status(500).json({ message: "Error Inesperado" });
+    console.error();
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    res.status(500).json({ message: "An error occurred while closing the session" });
+  }
+};
+
+export const profileUpdater = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { email, username } = req.body;
+    if (req.files?.media) {
+      const media = req.files.media;
+      if (media.mimetype !== "image/jpeg" && media.mimetype !== "image/png") return res.status(400).json({ message: "el formato de imágenes es invalida" });
+      const rout = media.tempFilePath;
+      const result = await uploadImage(rout);
+      const userUpdated = await user.findByIdAndUpdate(
+        id,
+        { $set: { emails: email, usernames: username, profilePicture: { _id: result.public_id, url: result.secure_url } } },
+        { new: true }
+      );
+      if (!userUpdated) return res.status(404).json({ message: "no se pudo realizar la actualización de su usuario " });
+      return res.status(200).json({ message: "perfil actualizado con éxito" });
+    } else {
+      const userUpdated = await user.findByIdAndUpdate(id, { $set: { emails: email, usernames: username } }, { new: true });
+      if (!userUpdated) return res.status(404).json({ message: "no se pudo realizar la actualización de su usuario " });
+      return res.status(200).json({ message: "perfil actualizado correctamente" });
+    }
+  } catch (error) {
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.log(color.red("                                An error occurred while updating the user"));
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    console.error();
+    console.error(error);
+    console.error();
+    console.log(color.blue("----------------------------------------------------------------------------------------------------"));
+    res.status(500).json({ message: "An error occurred while updating the user" });
   }
 };
--- backend/src/routers/user.routes.js
+++ backend/src/routers/user.routes.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { login, logout, register, secureAccess } from "../controllers/user.controllers.js";
+import { login, logout, register, secureAccess, profileUpdater } from "../controllers/user.controllers.js";
 import validatorJWT from "../middlewares/validatorJWT.js";
 import { loginValidations, registerValidations } from "../validations/validation.js";
 import { validator } from "../validator/validator.js";
@@ -10,5 +10,6 @@ userRouter.post("/register", registerValidations, validator, register);
 userRouter.post("/login", loginValidations, validator, login);
 userRouter.get("/session", validatorJWT, secureAccess);
 userRouter.post("/logout", logout);
+userRouter.put("/userUpdated", validatorJWT, profileUpdater);
 
 export default userRouter;
