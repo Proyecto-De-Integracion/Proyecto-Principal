56fac0b2d7ea16a84ef6ba423261fdcb5fcd67e1
LusCard <lucascardenas@outlook.es>
2024-10-02 14:43:55 -0300

    added vite url route

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,12 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["http://localhost:5500", "http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: [
+      "http://localhost:5500",
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
