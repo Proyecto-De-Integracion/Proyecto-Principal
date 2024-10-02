38d4307310d3fb7445575df191d01b1717c1b483
ElBrunoX.js <167462288+DaSilvaFelix@users.noreply.github.com>
2024-10-02 09:49:02 -0300

    Delete backend directory

--- backend/src/routers/medias.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { deleteImageInCloudinary } from "../controllers/media.controller.js";
-
-const mediaRouter = Router();
-mediaRouter.delete("/deleteImage", deleteImageInCloudinary);
-mediaRouter.delete("/");
-
-export default mediaRouter;
