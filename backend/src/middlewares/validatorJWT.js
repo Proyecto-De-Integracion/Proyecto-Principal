8f8b417f2c3435cb8f0b5edf8f42d7dbb7d7d57e
ElBrunoX.js <167462288+DaSilvaFelix@users.noreply.github.com>
2024-10-02 10:02:01 -0300

    solucion de conflictos

---
+++ backend/src/middlewares/validatorJWT.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config/config.js";
+import { user } from "../models/user.model.js";
+import color from "chalk";
+
+export default async (req, res, next) => {
+  try {
+    console.log("");
+    console.log("");
+    console.log("--------------------Session-------------------");
+    console.log(req.session);
+    console.log("----------------------------------------------");
+    console.log("");
+    console.log("");
+    console.log("--------------------cookies-------------------");
+    console.log(req.cookies);
+    console.log("----------------------------------------------");
+    console.log("");
+    console.log("");
+
+    const token = req.cookies.authToken || req.session.token;
+    if (!token) {
+      res.status(403).json({ message: "You do not have the authorization" });
+    }
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const userSearched = await user.findById(decoded.id);
+    if (!userSearched) {
+      res
+        .status(401)
+        .json({ message: "Your authorization has already expired" });
+    }
+    req.user = userSearched;
+    next();
+  } catch (error) {
+    console.log(" ");
+
+    console.log("---------------------Error---------------------");
+    console.log(color.red("Error al verificar el token"));
+    console.log("-----------------------------------------------");
+    console.log(color.yellow("El Usuario no tiene un Token "));
+    console.log("-----------------------------------------------");
+  }
+};
