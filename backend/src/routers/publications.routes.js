81b3541ded8707ed3c57afe5ba4973c95e10480e
Felix Polo <brunolok@gmail.com>
2024-10-03 14:13:01 -0300

    se corrigieron las routers en el conflicto

--- backend/src/routers/publications.routes.js
+++ backend/src/routers/publications.routes.js
@@ -1,16 +1,6 @@
 import { Router } from "express";
-import {
-  publicationGetter,
-  postCreator,
-  postFinderById,
-  postUpdater,
-  postRemover,
-  categoryPostGetter,
-  publicationGetterByTitle,
-} from "../controllers/publications.controllers.js";
+import { publicationGetter, postCreator, postFinderById, postUpdater, postRemover, categoryPostGetter, publicationGetterByTitle } from "../controllers/publications.controllers.js";
 import validatorJWT from "../middlewares/validatorJWT.js";
-import { publicationsValidations } from "../validations/validation.js";
-import { validator } from "../validator/validator.js";
 
 const publicationsRoutes = Router();
 publicationsRoutes.get("/publications", publicationGetter);
@@ -18,13 +8,7 @@ publicationsRoutes.get("/publications/:id", postFinderById);
 publicationsRoutes.post("/publications", validatorJWT, postCreator);
 publicationsRoutes.put("/publications/:id", validatorJWT, postUpdater);
 publicationsRoutes.delete("/publications/:id", postRemover);
-publicationsRoutes.get(
-  "/publications/searched/for/category/:category",
-  categoryPostGetter
-);
-publicationsRoutes.get(
-  "/publications/searched/for/title/:title",
-  publicationGetterByTitle
-);
+publicationsRoutes.get("/publications/searched/for/category/:category", categoryPostGetter);
+publicationsRoutes.get("/publications/searched/for/title/:title", publicationGetterByTitle);
 
 export default publicationsRoutes;
